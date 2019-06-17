f0a818f812331570e1aeaf265c8596b032e44a9f
Ghilia Weldesselasie <ghiliaweld@gmail.com>
2019-06-17 15:31:58 -0400

    added callback param to hypertale

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const hypertale = async (archive, path) => {
+const hypertale = async (archive, path, callback) => {
   const history = await archive.history()
   
-  const log = history.filter(change => change.path == path)             // changes at desired path
+  const promises = history.filter(change => change.path == path)             // changes at desired path
         .map(change => change.version)                                  // version of changes
         .map(version => archive.checkout(version))                      // old archives
         .map(async archive => JSON.parse(await archive.readFile(path))) // get data of old archives at path
   
-  const tale = await Promise.all(log)
-  return tale
+  const log = await Promise.all(promises)
   
+  callback(log) 
 }
 
 export default hypertale
