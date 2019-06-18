55150fcc12b2b7dd710d366b66e4589be1e0bcbb
Ghilia Weldesselasie <ghiliaweld@gmail.com>
2019-06-17 23:23:15 -0400

    wrote functions for array filter and map

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+const matchesPath = path => change => change.path == path
+const getVersion = change => change.version
+const retrieveArchives = archive => version => archive.checkout(version)
+const getFileAndParse = path => async archive => JSON.parse(await archive.readFile(path))
+
 const hypertale = async (archive, path, callback) => {
   const history = await archive.history()
   
-  const promises = history.filter(change => change.path == path)             // changes at desired path
-        .map(change => change.version)                                  // version of changes
-        .map(version => archive.checkout(version))                      // old archives
-        .map(async archive => JSON.parse(await archive.readFile(path))) // get data of old archives at path
+  const promises = history.filter(matchesPath(path))
+        .map(getVersion)
+        .map(retrieveArchives(archive))
+        .map(getFileAndParse(path))
   
   const log = await Promise.all(promises)
   
